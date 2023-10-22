2c5090a68c1b56bcd77a1047e83ac3df4a8eae40
Priyanshuhissaria <md.priyanshu@gmail.com>
2023-10-23 01:34:39 +0530

    created chess-board component

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ChessBoardComponent } from './components/chess-board/chess-board.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ChessBoardComponent
+  ],
   imports: [
     CommonModule
   ]
