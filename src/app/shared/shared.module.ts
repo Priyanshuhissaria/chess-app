a43a7aaf6cedcfef7eb626a2642a8c43e325a650
Priyanshuhissaria <md.priyanshu@gmail.com>
2023-10-24 17:01:25 +0530

    declaration and export of chess-board-component

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,12 +2,9 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ChessBoardComponent } from './components/chess-board/chess-board.component';
 
-
-
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  declarations: [ChessBoardComponent],
+  imports: [CommonModule],
+  exports: [ChessBoardComponent],
 })
 export class SharedModule {}
