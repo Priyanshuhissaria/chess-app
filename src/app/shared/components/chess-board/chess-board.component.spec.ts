eb06d406d09fba03f9266f4ac1096ca026609b54
Priyanshuhissaria <md.priyanshu@gmail.com>
2023-10-24 17:02:13 +0530

    indentation

--- src/app/shared/components/chess-board/chess-board.component.spec.ts
+++ src/app/shared/components/chess-board/chess-board.component.spec.ts
@@ -8,9 +8,8 @@ describe('ChessBoardComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ ChessBoardComponent ]
-    })
-    .compileComponents();
+      declarations: [ChessBoardComponent],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(ChessBoardComponent);
     component = fixture.componentInstance;
--- src/app/shared/components/chess-board/chess-board.component.ts
+++ src/app/shared/components/chess-board/chess-board.component.ts
@@ -1,16 +1,18 @@
 import { Component, OnInit } from '@angular/core';
-import { DEFAULT_INITIAL_BLACK_POSITION, DEFAULT_INITIAL_WHITE_POSITION } from '../../constants/chess-board.constant';
+import {
+  DEFAULT_INITIAL_BLACK_POSITION,
+  DEFAULT_INITIAL_WHITE_POSITION,
+} from '../../constants/chess-board.constant';
 
 @Component({
   selector: 'app-chess-board',
   templateUrl: './chess-board.component.html',
-  styleUrls: ['./chess-board.component.css']
+  styleUrls: ['./chess-board.component.css'],
 })
-export class ChessBoardComponent implements OnInit{
-  public initialPosition : string[][] = [];
-  constructor(){};
+export class ChessBoardComponent implements OnInit {
+  public initialPosition: string[][] = [];
+  constructor() {}
   ngOnInit(): void {
     this.initialPosition = DEFAULT_INITIAL_WHITE_POSITION;
   }
-
 }
